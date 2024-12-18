1887e050e45dd8d81d5cd0c19c9f245f27f0de39
PaoloGitAccount <58566249+PaoloGitAccount@users.noreply.github.com>
2024-12-18 18:05:16 +0000

    Create ExceptionMiddleware.cs

---
+++ TodoApi/src/webAPI/Controllers/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+namespace WebAPI.Middlewares;
+using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http; 
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var result = JsonSerializer.Serialize(new { error = exception.Message });
+        return context.Response.WriteAsync(result);
+    }
+}
